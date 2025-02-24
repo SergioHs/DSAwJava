8a652faebfdbf0fad67d8bb8c92c1710c4e7f257
SergioHs <shs_28@hotmail.com>
2025-02-24 18:18:49 -0300

    Removes definition of MergeTwoLists class

--- LinkedList/MergeTwoLists.java
+++ LinkedList/MergeTwoLists.java
@@ -54,22 +54,3 @@ public class MergeTwoLists {
     }
 
 }
-
-class ListNode{
-    int val;
-    ListNode next;
-
-    ListNode(int val){
-        this.val = val;
-        this.next = null;
-    }
-
-    static void printList(ListNode head){
-        System.out.println("List: ");
-        while(head != null){
-            System.out.println(head.val);
-            head = head.next;
-        }
-        System.out.println("(End)");
-    }
-}
